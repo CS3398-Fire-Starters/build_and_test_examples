22bbab89ea93598f95113ca62f627998a8ad26f4
tqblocker95 <tqb2@txstate.edu>
2017-04-05 18:37:27 -0500

    Work pls

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
+echo "testing hwInput_GP"
 python helloworld.py < hwInput_GP.txt
+echo "testing hwInput_GP_quoted"
 python helloworld.py < hwInput_GP_quoted.txt
+echo "testing hwInput_2"
 python helloworld.py < hwInput_2.txt
+echo "testing hwInput_2_quoted"
 python helloworld.py < hwInput_2_quoted.txt
